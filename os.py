2308851dd3909acd33980bb2adac17f7f7ab99d4
Diamantopoulos Stylianos <60236500+SteliosDiamantopoulos@users.noreply.github.com>
2022-12-07 19:49:34 +0100

    Add files via upload
    This Scrip organizes all documents in folders based on the document TYPE and the CODE given xxxx-CODE-xxxx.type

--- os.py
+++ os.py
@@ -4,75 +4,119 @@ import shutil
 
 
 
+def increment():
+    global COUNT
+    COUNT = COUNT+1
+    return COUNT
+
+
 
 def unique(list1):
     x=np.array(list1)
     return (np.unique(x))
 
-##Function that identifies file types from a certain directory
-def figure_file_types(source_dir):
-    file_type_list = []
-    for file in os.listdir():
-        file_type = file.split(".")[-1:]##Get the last element form the file name which is the type
-        file_type_list.append(file_type)
-    types = unique(file_type_list)#[0:(len(file_type_list))]
-    return(list(types))
+##Function that find all the unique projectNO in a folder of FILE TYPES
+def find_project_no (source):
+    #print(source)
+    projectList=[]
+    for file in os.listdir(source):
+        get_file_type = file.split('.')
+        #print((get_file_type))
+        if len(get_file_type)>1:
+            projectno=file.split('-')[1]
+            #print(projectno)
+            projectList.append(str(projectno))
+    #print(projectList)
+    return (projectList)
 
-##Move folders to created directories
-def create_target_files(source):
-    types = figure_file_types(source)
-    for ex in types:
-        try:
-            print(ex,end=",")
-            os.mkdir(source + "\\"+ex)
-            path = os.getcwd()
-            for file in os.listdir():
-                if ex in file :
-                    shutil.move(file,path+"\\"+ex)
-        except:
-            continue
 
-def rename_files_in_folder(source,user_input):
-    counter=0
-    for file in os.listdir():
-        print(file)
-        path = os.getcwd()
-        old_path = os.path.join(path,file)
-        if counter is 0:
-            customVariable = 'ONE'
-        elif counter is 1:
-            customVariable = 'TWO'
+##Function that create folder
+def create_targetfile(source):
+    projects = find_project_no(source)
+    #print(projects)
+    for project in projects:
+        os.makedirs(os.path.join(source,project),exist_ok=True)
+        
+##Function that moves the files from source to projectNO folders
+def move_files(source):
+    files=[]
+    folders=[]
+    for file in os.listdir(source):
+        get_file_type = file.split('.')
+        #print(len(get_file_type))
+        if len(get_file_type) == 2:
+            files.append(file)
         else:
-            customVariable = "THREE"
-        new_path = os.path.join(path,str(counter)+str(customVariable)+'.'+str(user_input))
-        counter+=1
-        os.rename(old_path,new_path)
-        
-        
+            folders.append(file)
+        #print(files)
+        #print(folders)
+    for file in files:
+        get_projectNO = file.split('-')[1]
+        create_source_path = os.path.join(source,file)
+        create_target_path = os.path.join(source,get_projectNO,file)
+        shutil.move(create_source_path,create_target_path)
 
-
-        
-    #print(oldname)
+##Function that generalizes the move_files for any file with the spesiffic format
+def manage_files_in_folder(userChoice):
+    #print(os.getcwd())
+    #path=os.path.join(os.getcwd())
     
+    path = os.path.join(os.getcwd(),userChoice)
+    create_targetfile(path)
+    move_files(path)
+##Function that split by folder type from main file
+def list_of_extensions(source):
+    os.chdir(source)
+    ex_list=[]
+    for file in os.listdir():
+        ex=file.split('.')
+        if len(ex)==2:
+            ex_list.append(ex[-1])
+    return ex_list
 
+
+
+def create_extension_files(source):
+    ex_list=list_of_extensions(source)
+    for ex in ex_list:
+        os.makedirs(os.path.join(source,ex),exist_ok=True)
+    
+        
+
+
+def move_files_to_extension(source):
+    files=[]
+    folders=[]
+    try:
+        for file in os.listdir(source):
+            for ex in list_of_extensions(source):
+                get_file_type = file.split('.')
+                    #print(len(get_file_type))
+                if len(get_file_type) == 2:
+                    files.append(file)
+                else:
+                    folders.append(file)
+                #print(files)
+                #print(folders)
+            
+            get_file_type = file.split('.')[-1]
+            create_source_path = os.path.join(source,file)
+            create_target_path = os.path.join(source,str(get_file_type)+'\\'+file)
+            print(create_target_path)
+            shutil.copy(create_source_path,create_target_path)
+    except:
+        pass
 def main():
-    path = os.getcwd()
-    userchoice='png'
-    a = path + "\\" + userchoice
-    source_dir = os.chdir(a)
-    rename_files_in_folder(source_dir,userchoice)
-     #create_target_files(source_dir)
-
-
+    #1st sort the new entries
+    ##The app should be in the same folder as database
+    path=os.getcwd()
+    create_extension_files(path)
+    move_files_to_extension(path)
+    userChoices = unique(list_of_extensions(path))
+    for userChoice in userChoices:
+        manage_files_in_folder(userChoice)
+    #move_files(path)
+    
 if __name__=='__main__':
-    ##Chosse location for the script to be applied
-    #source_dir = r"C:\Users\stdia\Desktop\PythonScripting\data"
-    ##Make sure that the directory is the chosen one
-    #path = os.getcwd()
-    #userchoice=str(input("Enter File_Name TO Operate On:"))
-    #userchoice='png'
-    #a = path + "\\" + userchoice
-    # source_dir = os.chdir(a)
-    # print(source_dir)
     main()
-
+    
